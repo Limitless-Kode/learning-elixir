a4ce9355c3bc0a459d27d09fd8c5e8053ecba66a
Peter Claver Amobila <limitless.claver@gmail.com>
2021-05-30 04:10:24 +0000

    Learnt how documentation is done in elixir

---
+++ lib/Math.ex
@@ -0,0 +1,17 @@
+defmodule Math do
+  @moduledoc """
+  Documentation for the math module
+  """
+
+  @doc """
+  adding two numbers
+
+  ## Examples
+  iex> Math.add(3,5)
+  :8
+
+  """
+  def add(x,y) do
+    x + y
+  end
+end
